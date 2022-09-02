4f0c8d81dfb2660e7c1ddb3657427c8b96c408a9
Vanyhony Esneyder Zabala Baez <vezabala@misena.edu.co>
2022-09-02 10:44:08 -0500

    Chore: Actividad 6 clases genericas

---
+++ Actividades/Actividad6/Actividad6/Homes/AnimalFoundation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Actividad6.Entitys;
+
+namespace Actividad6.Homes
+{
+    internal class AnimalFoundation<T> : IHome<T> where T : Entidad
+    {
+        private List<T> InscribedsFoundation { get; set; }
+
+        public AnimalFoundation()
+        {
+            InscribedsFoundation = new();
+        }
+
+        public void Enroll(T obj)
+        {
+            obj.Id = "1";
+
+            InscribedsFoundation.Add(obj);
+        }
+    }
+}
